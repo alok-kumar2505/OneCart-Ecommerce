82406ba30daaa83e402326a8083f406017fe3eef
Alok Kumar <148738056+2784alok@users.noreply.github.com>
2025-12-20 15:50:23 +0530

    Modify multer storage to include timestamp in filename
    Updated filename function to prepend timestamp.

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,14 @@
-import multer from 'multer'
+import multer from "multer";
 
 let storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./public")
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./public");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
- let upload = multer({storage})
 
- export default upload
+let upload = multer({ storage });
+
+export default upload;
